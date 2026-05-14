968ef411d9c61a72a3da08f23a71105c36b0640b
Sabrina Tang <sabrit4@uci.edu>
2026-05-14 10:24:16 -0700

    leetcode practice

--- two_sum.py
+++ two_sum.py
@@ -5,3 +5,20 @@ def twoSum(nums, k):
             return (mp[k-nums[i]], i)
         mp[nums[i]] = i
     return -1
+
+# Two Integer Sum II
+from typing import List
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        n = len(numbers)
+        while l < r:
+            # check if current pair returns target
+            if numbers[l] + numbers[r] == target:
+                return [l + 1, r + 1]
+            # if l
+            elif numbers[l] + numbers[r] > target:
+                r -= 1
+            else:
+                l += 1
+        return [l, r]
